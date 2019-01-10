4f98cd8f63e060ab0a8718162e7b63a0df7e41ea
Edwin Benitez <edwinbenitez3216@gmail.com>
2019-01-10 12:25:55 -0800

    Uploading db file

---
+++ connectWithDB.py
@@ -0,0 +1,10 @@
+class Mydba:
+  
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+
+
